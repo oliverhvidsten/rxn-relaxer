b2b02da7b42e877502178d426aa1880708e7bf0a
oliverhvidsten <oliverhvidsten@gmail.com>
2025-09-23 19:09:37 -0700

    added files for the to-be-implemented functions and included description of intended functionality

---
+++ rxnrlx/diagram.py
@@ -0,0 +1,7 @@
+"""
+TODO: 
+This file should take in molecules and energies in a form 
+similar to what refine.py outputs and output a reaction diagram.
+
+It should be possible to chain reactions together as well
+"""
