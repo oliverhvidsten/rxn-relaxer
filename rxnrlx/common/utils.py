4618293062b9782b24aaa3e7607483b80dcd290e
oliverhvidsten <oliverhvidsten@gmail.com>
2025-09-29 22:53:32 -0400

    consolidate utility functions

--- rxnrlx/common/utils.py
+++ rxnrlx/common/utils.py
@@ -21,3 +21,21 @@ def sec_to_str(seconds):
 
 
 
+def create_submit_script(account_info:dict, command:str):
+    """
+    Create submit script from user-specified inputs
+    """
+
+
+    submit_script = ["#!/bin/bash -l", ""]
+
+    # iterate through the specified parameters and build the script
+    for key, val in account_info:
+        submit_script.append(f"#SBATCH --{key}={val}")
+    
+    # append final command
+    submit_script.append("")
+    submit_script.append(command)
+
+    with open("submit.script", "w") as f: 
+        f.write("\n".join(submit_script))
